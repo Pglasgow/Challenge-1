51021c4ac9cbab26f8ea4aa51de3518c870adfec
Pglasgow <pglasgow02@qub.ac.uk>
2024-09-23 16:37:26 +0100

    Add files via upload

--- PlayerControllerX.cs
+++ PlayerControllerX.cs
@@ -13,9 +13,11 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 5.0f;
     public float rotationSpeed;
     public float verticalInput;
+
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +26,16 @@ public class PlayerControllerX : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
+        forwardInput = Input.GetAxis("Horizontal");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
